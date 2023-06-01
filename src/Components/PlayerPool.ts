50eba8ffe87ec76596a1d9cd3ccf272b9979a25e
jtran0721 <janetran24@outlook.com>
2023-06-01 20:22:35 +1000

    Installed MUi and remove tailwinds

---
+++ src/Components/PlayerPool.ts
@@ -0,0 +1,19 @@
+interface NewPlayer {
+  firstname: string;
+  lastname: string;
+  level: number;
+}
+
+export const playerPool: NewPlayer[] = [];
+
+const addPlayer = function (
+  firstname: string,
+  lastname: string,
+  level: number
+): void {
+  const player: NewPlayer = { firstname, lastname, level };
+  playerPool.push(player);
+};
+
+addPlayer("Jane", "Tran", 5);
+console.log(playerPool);
